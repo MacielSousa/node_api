3fd5d78290a13a47837fca0e042714c9947added
macielsousa <fmacielsousa@gmail.com>
2021-10-10 16:32:29 -0300

    fix: edit views e DAO e routers

--- app/routes/admin.js
+++ app/routes/admin.js
@@ -1,14 +1,24 @@
 module.exports = function(application){
     application.get('/formulario_inclusao_noticia', function(req, res){
-        res.render("admin/form_add_noticia");
+        res.render("admin/form_add_noticia", { validacao : {}, noticia : {}});
     });
     application.post('/noticias/salvar', function(req, res){
         const noticia = req.body;
 
-        //Conexão
-        //Model
+        console.log(noticia);
+        req.assert('titulo', 'Título é obrigatório').notEmpty();
+        req.assert('resumo', 'Resumo é obrigatório').notEmpty();
+        req.assert('resumo', 'Resumo deve conter entre 10 e 100 caracteres').len(10, 100);
+        req.assert('autor', 'Autor é obrigatório').notEmpty();
+        req.assert('data_noticia', 'Data é obrigatório').notEmpty().isDate({format: 'YYYY-MM-DD'});
+        req.assert('autor', 'Noticia pe obrigatório').notEmpty();
 
-        //SalvarNoticias
+        const erros = req.validationErrors();
+        if(erros){
+            console.log(noticia)
+            res.render("admin/form_add_noticia", {validacao : erros, noticia: noticia} );
+            return;
+        }
 
         const connection = application.config.dbConnection();
         const noticiasModel = new application.app.models.NoticiasDAO(connection);
--- config/server.js
+++ config/server.js
@@ -1,12 +1,15 @@
 const express = require('express');
 const consig = require('consign');
 const bodyParser = require('body-parser');
+const expressValidator = require('express-validator');
 
 const app = express();
 app.set('view engine', 'ejs');
 app.set('views', './app/views');
 
+
 app.use(bodyParser.urlencoded({extended: true}));
+app.use(expressValidator());
 
 consig()
     .include('app/routes')
